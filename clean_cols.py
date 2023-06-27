6fac17a7261399e51068f1de8178f76c41f92345
Luca Battistella <luca.battistella.geo@gmail.com>
2023-06-27 08:58:23 +0100

    Update clean_cols.py

--- clean_cols.py
+++ clean_cols.py
@@ -7,6 +7,7 @@ folder_path = "/Users/lucabattistella/Downloads/tmf"
 # Get the list of files in the folder
 file_list = os.listdir(folder_path)
 
+    
 # Iterate over each file
 for file_name in file_list:
     # Check if the file is a CSV file
@@ -30,3 +31,29 @@ for file_name in file_list:
             df.to_csv(file_path, index=False)
         else:
             print(f"Columns not found in file: {file_name}")
+
+
+# Get the list of CSV files in the folder
+file_list = [file for file in os.listdir(folder_path) if file.endswith(".csv")]
+
+# Iterate over each CSV file
+for file_name in file_list:
+    # Read the CSV file into a pandas DataFrame
+    file_path = os.path.join(folder_path, file_name)
+    df = pd.read_csv(file_path)
+    
+    # Extract the file name without extension
+    file_name_without_ext = os.path.splitext(file_name)[0]
+    
+    # Define columns to exclude from renaming
+    excluded_columns = ["isoa3_id", "isoa3_id_1", "protection"]
+    
+    # Iterate over each column
+    for column in df.columns:
+        if column not in excluded_columns:
+            # Rename the column by adding file name and "_"
+            new_column_name = file_name_without_ext + "_" + column
+            df.rename(columns={column: new_column_name}, inplace=True)
+    
+    # Save the modified DataFrame back to the CSV file
+    df.to_csv(file_path, index=False)
